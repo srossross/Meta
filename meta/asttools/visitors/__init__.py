5489cedda19ba2e53569e659891b72677c46d0a4
srossross <srossross@enthought.com>
2011-12-07 07:39:05 -0600

    ADDED: mutator class.

--- meta/asttools/visitors/__init__.py
+++ meta/asttools/visitors/__init__.py
@@ -47,3 +47,41 @@ class Visitor(object):
             return mehtod(node, *args, **kwargs)
 
 
+
+class Mutator(Visitor):
+    
+    def mutateDefault(self, node):
+        for field in node._fields:
+            value = getattr(node, field)
+            if isinstance(value, (list, tuple)):
+                for i, item in enumerate(value):
+                    if isinstance(item, _ast.AST):
+                        new_item = self.mutate(item)
+                        if new_item is not None:
+                            value[i] = new_item 
+                    else:
+                        pass
+                    
+            elif  isinstance(value, _ast.AST):
+                new_value = self.mutate(value)
+                if new_value is not None:
+                    setattr(node, field, new_value)
+
+        return None
+    
+    def mutate(self, node, *args, **kwargs):
+        
+        node_name = type(node).__name__
+
+        attr = 'mutate' + node_name
+
+        if hasattr(self, attr):
+            mehtod = getattr(self, 'mutate' + node_name)
+            return mehtod(node, *args, **kwargs)
+        elif hasattr(self, 'mutateDefault'):
+            mehtod = getattr(self, 'mutateDefault')
+            return mehtod(node, *args, **kwargs)
+        else:
+            mehtod = getattr(self, 'mutate' + node_name)
+            return mehtod(node, *args, **kwargs)
+
