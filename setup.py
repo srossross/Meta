4dbed535461db3501bdb1804709318a4df7ded85
srossross <srossross@enthought.com>
2012-01-02 10:34:16 -0800

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meta',
-    version='0.2.0',
+    version='0.2.1',
     author='Sean Ross-Ross, Enthought Inc.',
     author_email='srossross@enthought.com',
     maintainer='Sean Ross-Ross',
